d53aeec2deaa12d2950962e15c8e92ef43adb7f6
Brandon Ragghianti <brandonragghianti@gmail.com>
2024-12-01 19:27:16 -0800

    Pushed update Query.java.

--- src/main/java/lambda/Query.java
+++ src/main/java/lambda/Query.java
@@ -4,6 +4,7 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.sql.Connection;
@@ -12,9 +13,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Properties;
+import java.util.*;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 import saaf.Inspector;
@@ -45,6 +45,7 @@ public class Query implements RequestHandler<HashMap<String, Object>, HashMap<St
         // Step 1: Parse the JSON to receive aggregations and filters.
         final JSONArray aggregations = jsonRequest.optJSONArray("aggregations");
         final JSONArray filters = jsonRequest.optJSONArray("filters");
+        final JSONArray group = jsonRequest.optJSONArray("group");
 
         // Create the start of the SQL query.
         final StringBuilder sqlQuery = new StringBuilder("SELECT ");
@@ -83,6 +84,18 @@ public class Query implements RequestHandler<HashMap<String, Object>, HashMap<St
             }
         }
 
+        if (group != null && !group.isEmpty()) {
+            sqlQuery.append(" GROUP BY ");
+            for (int i = 0; i < group.length(); i++) {
+                final String value = group.getString(i);
+                values.add(value);
+                sqlQuery.append("?");
+                if (i != group.length() - 1) {
+                    sqlQuery.append(", ");
+                }
+            }
+        }
+
         sqlQuery.append(";");
 
         System.out.println(sqlQuery.toString());
@@ -108,7 +121,7 @@ public class Query implements RequestHandler<HashMap<String, Object>, HashMap<St
             throw new RuntimeException(e);
         }
 
-        final JSONArray jsonResult = new JSONArray();
+        final ArrayList<JSONObject> result = new ArrayList<>();
         try {
             final PreparedStatement dbTableSelect = con.prepareStatement(sqlQuery.toString());
             for (int i = 0; i < values.size(); i++) {
@@ -123,18 +136,91 @@ public class Query implements RequestHandler<HashMap<String, Object>, HashMap<St
                     final String columnName = rsmd.getColumnName(i);
                     obj.put(columnName, rs.getObject(columnName));
                 }
-                jsonResult.put(obj);
+                logger.log(obj.toString());
+                result.add(obj);
             }
         } catch (final SQLException e) {
             System.out.println("Failed to query database:");
             throw new RuntimeException(e);
         }
-        inspector.addAttribute("entries", jsonResult.toString());
+        inspector.addAttribute("entries", result);
 
         //****************END FUNCTION IMPLEMENTATION***************************
 
         //Collect final information such as total runtime and cpu deltas.
         inspector.inspectAllDeltas();
         return inspector.finish();
+    }
+
+    public static void main(final String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("json.json"));
+        String request = "";
+        while (scanner.hasNextLine()) {
+            request += scanner.nextLine();
+        }
+
+        // Turn AWS request object to proper json.
+        final JSONObject jsonRequest = new JSONObject(request);
+
+        // Step 1: Parse the JSON to receive aggregations and filters.
+        final JSONArray aggregations = jsonRequest.optJSONArray("aggregations");
+        final JSONArray filters = jsonRequest.optJSONArray("filters");
+        final JSONArray group = jsonRequest.optJSONArray("group");
+
+        // Create the start of the SQL query.
+        final StringBuilder sqlQuery = new StringBuilder("SELECT ");
+
+        // Step 2: Parse the aggregations part of the json and add to select.
+        if (aggregations != null && !aggregations.isEmpty())  {
+            for (int i = 0; i < aggregations.length(); i++) {
+                final JSONObject aggregation = aggregations.getJSONObject(i);
+                final String column = aggregation.getString("column");
+                final String function = aggregation.getString("function");
+                sqlQuery.append(function).append("(").append(column).append(") AS ").append(function).append("_").append(column);
+                if (i != aggregations.length() - 1) {
+                    sqlQuery.append(", ");
+                }
+            }
+        } else {
+            sqlQuery.append(" * ");
+        }
+
+        sqlQuery.append(" FROM data ");
+
+        // Create ArrayList to store values instead of directly adding to query to prevent SQL injection.
+        final ArrayList<String> values = new ArrayList<>();
+
+        // Step 3: Build the WHERE clause (with Filters)
+        if (filters != null && !filters.isEmpty()) {
+            sqlQuery.append("WHERE ");
+            for (int i = 0; i < filters.length(); i++) {
+                final JSONObject filter = filters.getJSONObject(i);
+                final String column = filter.getString("column");
+                values.add(filter.getString("value"));
+                sqlQuery.append(column).append(" = ?");
+                if (i != filters.length() - 1) {
+                    sqlQuery.append(" AND ");
+                }
+            }
+        }
+
+        if (group != null && !group.isEmpty()) {
+            sqlQuery.append(" GROUP BY ");
+            for (int i = 0; i < group.length(); i++) {
+                final String value = group.getString(i);
+                values.add(value);
+                sqlQuery.append("?");
+                if (i != group.length() - 1) {
+                    sqlQuery.append(", ");
+                }
+            }
+        }
+
+        sqlQuery.append(";");
+
+        System.out.println(sqlQuery.toString());
+
+        System.out.println(values.toString());
     }
 }
